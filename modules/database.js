32dac7e8de9cc90fcabe9c18b66b30d47f7631d3
jossafer96 <jossafer96@gmail.com>
2019-10-10 00:32:04 -0600

    Update database.js

--- modules/database.js
+++ modules/database.js
@@ -5,7 +5,7 @@ var db = 'driveRack';
 class Database{
     constructor(){
         
-        mongoose.connect('mongodb+srv://jossa:jossa@cluster0-ulxhy.mongodb.net/test?retryWrites=true&w=majority')
+        mongoose.connect('mongodb+srv://asd:asd@cluster0-ulxhy.mongodb.net/test?retryWrites=true&w=majority')
         .then(()=>{
             console.log('Se conecto a Mongo');
         }).catch((error)=>{
